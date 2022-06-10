a3a740540c083de80d74567765426d6359a16f38
Walid <dw_boubechtoula@esi.dz>
2022-06-10 09:20:49 +0100

    methode that return current thread code.

--- src/main/java/functional/Lab1.java
+++ src/main/java/functional/Lab1.java
@@ -1,15 +1,23 @@
 package functional;
 
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class Lab1 {
     public static void main(String[] args) throws Exception {
-        Callable<String> getThreadCode = () -> {
-            int randomNumber = (int) (Math.random() * ((1024 - 1) + 1));
-            return new StringBuilder().append("t").append(Thread.currentThread().getId()).append("-").append(randomNumber).toString();
-        };
-
+        System.out.println("Résultat de la premiere function");
         System.out.println(getThreadCode.call());
+        System.out.println("Résultat de la deuxieme function");
+        System.out.println(calculateLenght.apply("java is awesome"));
     }
 
+   static Callable<String> getThreadCode = () -> {
+        int randomNumber = (int) (Math.random() * ((1024 - 1) + 1));
+        return new StringBuilder().append("t").append(Thread.currentThread().getId()).append("-").append(randomNumber).toString();
+    };
+
+    static Function<String, Integer> calculateLenght = (String str) -> {
+        if (str == null) return -1;
+        return str.length();
+    };
 }
