7ac3bdf12f6119e8baff9c11a9191b7f3ec84f58
Walid <dw_boubechtoula@esi.dz>
2022-06-10 08:58:22 +0100

    Initial commit

---
+++ src/main/java/org/walid/reactivespringstarter/ReactiveSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package org.walid.reactivespringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringStarterApplication.class, args);
+    }
+
+}
