d515d2e1a1356090116aec097a7b0d885104e636
Walid <dw_boubechtoula@esi.dz>
2022-06-12 17:50:34 +0100

    lab8

--- src/main/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7.java
+++ src/main/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7.java
@@ -1,38 +1,16 @@
 package com.soprasteria.innersource.reactiveprogramming.reactive.lab7;
 
 import reactor.core.publisher.Flux;
-import reactor.util.function.Tuples;
 
 import java.time.Duration;
 
 public class Lab7 {
-    public static void main(String[] args) {
-        System.out.println(Long.toHexString(20L));
-    }
 
     public static Flux<String> numbers() {
-        return Flux.interval(Duration.ofMillis(50))
-                .startWith(200L)
-                .filter(integer -> integer > 20 && (integer % 2 == 0))
+        return Flux.range(200, 2000)
+                .interval(Duration.ofMillis(50))
+                .filter(integer -> integer % 2 == 0)
                 .map(aLong -> Long.toHexString(aLong.longValue()))
-                .take(5)
-                .log();
-
-
-    }
-
-    /**
-     * from baeldung web site
-     *
-     * @return
-     */
-    private static Flux<Integer> generateFibonacciWithTuples() {
-        return Flux.generate(
-                () -> Tuples.of(0, 1),
-                (state, sink) -> {
-                    sink.next(state.getT1());
-                    return Tuples.of(state.getT2(), state.getT1() + state.getT2());
-                }
-        );
+                .take(5);
     }
 }
