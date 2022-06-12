8592323397b83d3ac8944fd99478ca4551ee6861
Walid <dw_boubechtoula@esi.dz>
2022-06-12 13:41:56 +0100

    methode that return current thread code.

--- src/main/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7.java
+++ src/main/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7.java
@@ -1,23 +1,38 @@
 package com.soprasteria.innersource.reactiveprogramming.reactive.lab7;
 
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
 
 import java.time.Duration;
-import java.util.Random;
 
 public class Lab7 {
-
+    public static void main(String[] args) {
+        System.out.println(Long.toHexString(20L));
+    }
 
     public static Flux<String> numbers() {
-        var random = new Random();
-        Flux<Integer> fluxNumbers = Flux.create((fluxSink -> {
-            for (int i = 1; i <= 5; i++) {
-                fluxSink.next(random.nextInt());
-            }
-        }));
-        return fluxNumbers.filter(integer -> integer % 2 == 0)
-                .map(integer -> Integer.toHexString(integer.intValue()))
-                .delayElements(Duration.ofMillis(50));
+        return Flux.interval(Duration.ofMillis(50))
+                .startWith(200L)
+                .filter(integer -> integer > 20 && (integer % 2 == 0))
+                .map(aLong -> Long.toHexString(aLong.longValue()))
+                .take(5)
+                .log();
 
+
+    }
+
+    /**
+     * from baeldung web site
+     *
+     * @return
+     */
+    private static Flux<Integer> generateFibonacciWithTuples() {
+        return Flux.generate(
+                () -> Tuples.of(0, 1),
+                (state, sink) -> {
+                    sink.next(state.getT1());
+                    return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+                }
+        );
     }
 }
--- src/test/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7Test.java
+++ src/test/java/com/soprasteria/innersource/reactiveprogramming/reactive/lab7/Lab7Test.java
@@ -3,18 +3,14 @@ package com.soprasteria.innersource.reactiveprogramming.reactive.lab7;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+
 class Lab7Test {
 
     @Test
     void shouldReturn5Element() {
-        var elements = Lab7.numbers();
-        int[] cpt = new int[1];
-        cpt[0] = 0;
-        elements
-                .doOnNext(s -> ++cpt[0])
-                .doOnComplete(() -> {
-                    Assertions.assertTrue(cpt[0] == 5);
-                });
+        var count = Lab7.numbers().toStream().collect(Collectors.counting());
+        Assertions.assertTrue(count == 5L);
     }
 
 }
